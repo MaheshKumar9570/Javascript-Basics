e59acf8555099625564c78cb88e82ffdfbd8e27e
MaheshKumar9570 <163812123+MaheshKumar9570@users.noreply.github.com>
2024-03-18 17:32:48 +0000

    datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,24 @@
+const accountId=1232
+let accountEmail="mahesh@gmail.com"
+var accountPassword="12354"
+accountCity="jamshedpur"
+
+
+// accountId=1292 // It is not allow
+/*
+prefer not to use var
+becase of issues in block scope  and functional scope
+
+*/
+accountEmail="mk@gmail.com"
+accountPassword="12394"
+accountCity="Ranchi"
+
+
+
+console.log("accountId")
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+//alert(3+4)//we are using nodejs, not browser
+console.log(3
+    +
+    4)//code readbility should be high
+    console.log("Hi Mahesh ");
+
+    let name="Mahesh"
+    let age=22
+    let isLoggedIn = false
+    let state;
+
+    //number => to power 53
+    // bight
+    //string =>""
+    //bolean=> true/false
+    //null=> standalone value
+    //undefined => 
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof "Mahesh")
