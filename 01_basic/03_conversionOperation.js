14900f1f9a5aebc836a26909427f14f5f602ab96
MaheshKumar9570 <163812123+MaheshKumar9570@users.noreply.github.com>
2024-03-18 17:59:01 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "88"
+console.log(typeof (score));
+console.log (typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "Mahesh" 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 =>false
+//""=>false
+//"mahesh"=> true
+
